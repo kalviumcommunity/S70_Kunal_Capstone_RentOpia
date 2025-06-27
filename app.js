d0c8920450df11e83362a1aada9105506b696449
kunalyadav47 <kunal.yadav.s70@kalvium.community>
2025-06-28 03:12:36 +0530

    Update app.js for render

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const propertyRoutes = require('./routes/properties');
 
-dotenv.config(); // Load variables from .env
+dotenv.config();
 
 const app = express();
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// MongoDB Atlas connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
@@ -19,10 +17,12 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('MongoDB connection error:', err);
   });
 
-// API Routes
+app.get('/', (req, res) => {
+  res.send('RentOpia Backend is Live');
+});
+
 app.use('/api', propertyRoutes);
 
-// Port setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
